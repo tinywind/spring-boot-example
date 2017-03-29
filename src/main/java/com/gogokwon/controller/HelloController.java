3de7f85f501bbb9486d76a1cb9a99a0fdbf59386
tinywind <tinywind0@gmail.com>
2017-03-29 20:40:31 +0900

    pom.xml jsp 적용

--- src/main/java/com/gogokwon/controller/HelloController.java
+++ src/main/java/com/gogokwon/controller/HelloController.java
@@ -1,14 +1,15 @@
 package com.gogokwon.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("hello")
-public class HelloController{
-    @RequestMapping
-    public String indexPage(){
+public class HelloController {
+    @RequestMapping("")
+    public String indexPage(Model model) {
+        model.addAttribute("name", "gogokwon");
         return "index";
     }
-
 }
